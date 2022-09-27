561902d13ac2703b5cfa8003625c3ef46485d601
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2022-09-27 21:08:04 +0400

    project completed

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
 } from "react-native";
 import GoalInput from "./components/GoalInput";
 import GoalItem from "./components/GoalItem";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   
@@ -35,46 +36,49 @@ export default function App() {
       });
   }
   return (
-    <View style={styles.appContainer}>
-      <Button title="Add New Goal" color="red" onPress={startAddGoalHandler} />
-      <GoalInput
-        onAddGoal={addGoalHandler}
-        onCancel={endAddGoalHandler}
-        visible={modalIsVisible}
-      />
-      <View style={styles.goalsContainer}>
-        <View style={styles.container}>
-          <FlatList
-            data={goals}
-            renderItem={(itemData) => (
-              <GoalItem
-                text={itemData.item.text}
-                id={itemData.item.id}
-                onDeleteItem={deleteItemHandler}
-              />
-            )}
-            keyExtractor={(item) => {
-              return item.id;
-            }}
-          />
+    <>
+      <StatusBar style="light" />
+      <View style={styles.appContainer}>
+        <Button
+          title="Add New Goal"
+          color="red"
+          onPress={startAddGoalHandler}
+        />
+        <GoalInput
+          onAddGoal={addGoalHandler}
+          onCancel={endAddGoalHandler}
+          visible={modalIsVisible}
+        />
+        <View style={styles.goalsContainer}>
+          <View style={styles.container}>
+            <FlatList
+              data={goals}
+              renderItem={(itemData) => (
+                <GoalItem
+                  text={itemData.item.text}
+                  id={itemData.item.id}
+                  onDeleteItem={deleteItemHandler}
+                />
+              )}
+              keyExtractor={(item) => {
+                return item.id;
+              }}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 22,
-  },
   appContainer: {
     paddingTop: 50,
     paddingHorizontal: 16,
     flex: 1,
+    backgroundColor: "#1e085a",
   },
   goalsContainer: {
     flex: 5,
   },
-  
 });
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import {TextInput,View,TouchableOpacity,Text,StyleSheet,Modal, Button} from 'react-native';
+import {TextInput,View,StyleSheet,Modal, Button,Image} from 'react-native';
 
 
 export default function GoalInput(props) {
@@ -16,6 +16,13 @@ export default function GoalInput(props) {
     }
   return (
     <Modal visible={props.visible} animationType="fade">
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/goal1.png")}
+        />
+      </View>
+
       <View style={styles.inputContainer}>
         <TextInput
           onChangeText={goalInputHandler}
@@ -25,10 +32,10 @@ export default function GoalInput(props) {
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Add Goal" onPress={addGoalHandler} />
+            <Button title="Cancel" onPress={props.onCancel} color="#f31282" />
           </View>
           <View style={styles.button}>
-            <Button title='Cancel' onPress={props.onCancel}/>
+            <Button title="Add Goal" onPress={addGoalHandler} color="#b180f0" />
           </View>
         </View>
       </View>
@@ -40,27 +47,39 @@ const styles = StyleSheet.create({
   inputContainer: {
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
     flex: 1,
-    marginHorizontal:15
+    padding: 16,
+    backgroundColor: "#311b6b",
+  
   },
   inputText: {
     borderWidth: 1,
-    borderColor: "#cccccc",
+    borderColor: "#e4d0ff",
+    backgroundColor:'#e4d0ff',
+    color:"#120438",
     width: "100%",
-    padding: 8,
-    paddingVertical:13,
-    borderRadius: 5,
+    padding: 16,
+    paddingVertical: 13,
+    borderRadius: 6,
   },
-  buttonContainer:{
-    flexDirection:'row',
-    marginTop:16
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 16,
   },
   button: {
-    width:100,
-    marginHorizontal:8,
-
+    width: 100,
+    marginHorizontal: 8,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
+  },
+  imageContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 25,
+    backgroundColor: "#311b6b",
   },
 });
